cf984cb247749975e34b0c24a68844634e491387
João Vitor Soares <jvfs12th@gmail.com>
2024-07-19 15:27:41 -0300

    refactor: Adiciona código boilerplate em Cliente

--- src/main/java/com/dd/descontodiretoapi/models/Cliente.java
+++ src/main/java/com/dd/descontodiretoapi/models/Cliente.java
@@ -15,4 +15,43 @@ public class Cliente {
     private String senha;
     private String telefone;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
