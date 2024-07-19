191ec62447e687967a387b6d08e6498eb2c2ae15
João Vitor Soares <jvfs12th@gmail.com>
2024-07-19 14:48:28 -0300

    init: Inicia projeto

---
+++ src/main/java/com/dd/descontodiretoapi/DescontodiretoapiApplication.java
@@ -0,0 +1,13 @@
+package com.dd.descontodiretoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescontodiretoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescontodiretoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dd/descontodiretoapi/DescontodiretoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.descontodiretoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescontodiretoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
