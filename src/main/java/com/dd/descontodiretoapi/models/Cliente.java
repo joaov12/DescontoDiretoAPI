5ad00635bd60e02d8bc53692e6d18c3e92a4ac55
João Vitor Soares <jvfs12th@gmail.com>
2024-07-19 15:12:21 -0300

    build: Cria modelo de funcionario

---
+++ src/main/java/com/dd/descontodiretoapi/models/Cliente.java
@@ -0,0 +1,18 @@
+package com.dd.descontodiretoapi.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+
+}
