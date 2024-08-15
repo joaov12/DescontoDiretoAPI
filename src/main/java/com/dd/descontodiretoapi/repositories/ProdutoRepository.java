9d98b105edab962f9f721a91835b16d2bc7116b4
João Vitor Soares <jvfs12th@gmail.com>
2024-08-14 21:57:02 -0300

    refactor: Adiciona anotação de repository

--- src/main/java/com/dd/descontodiretoapi/repositories/ProdutoRepository.java
+++ src/main/java/com/dd/descontodiretoapi/repositories/ProdutoRepository.java
@@ -2,6 +2,8 @@ package com.dd.descontodiretoapi.repositories;
 
 import com.dd.descontodiretoapi.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
