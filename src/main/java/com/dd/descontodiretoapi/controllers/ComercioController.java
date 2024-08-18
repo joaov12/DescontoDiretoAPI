44c35a15a9f02750a1fb20047cdfb7a552333b3d
João Vitor Soares <jvfs12th@gmail.com>
2024-08-18 11:20:05 -0300

    refactor: Adiciona informações de resposta para endpoints de comercio

--- src/main/java/com/dd/descontodiretoapi/controllers/ComercioController.java
+++ src/main/java/com/dd/descontodiretoapi/controllers/ComercioController.java
@@ -1,9 +1,11 @@
 package com.dd.descontodiretoapi.controllers;
 
 import com.dd.descontodiretoapi.models.Comercio;
-import com.dd.descontodiretoapi.services.ClienteService;
 import com.dd.descontodiretoapi.services.ComercioService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,31 +22,131 @@ public class ComercioController {
     @Autowired
     private ComercioService comercioService;
 
-    @Operation(summary = "Obter todos os comercios", description = "Retorna uma lista de todos os comercios cadastrados")
+    @Operation(
+            summary = "Obter todos os comércios",
+            description = "Retorna uma lista de todos os comércios cadastrados",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Lista de comércios recuperada com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Comercio.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno no servidor",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @GetMapping("/all")
     public ResponseEntity<List<Comercio>> getAllComercios() {
         return ResponseEntity.status(HttpStatus.OK).body(comercioService.findAll());
     }
 
-    @Operation(summary = "Obter Comercio por ID", description = "Retorna um Comercio com base no ID fornecido")
+    @Operation(
+            summary = "Obter Comercio por ID",
+            description = "Retorna um Comercio com base no ID fornecido",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Comercio encontrado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Comercio.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Comercio não encontrado",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno no servidor",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @GetMapping("/find/{id}")
     public ResponseEntity<Comercio> getComercioById(@PathVariable("id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(comercioService.findById(id));
     }
 
-    @Operation(summary = "Adicionar Comercio", description = "Adiciona um novo Comercio")
+    @Operation(
+            summary = "Adicionar Comercio",
+            description = "Adiciona um novo Comercio",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Comercio adicionado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Comercio.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Requisição inválida",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno no servidor",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @PostMapping("/add")
     public ResponseEntity<Comercio> addComercio(@RequestBody Comercio comercio) {
         return ResponseEntity.status(HttpStatus.CREATED).body(comercioService.addComercio(comercio));
     }
 
-    @Operation(summary = "Editar Comercio", description = "Edita um Comercio existente")
+    @Operation(
+            summary = "Editar Comercio",
+            description = "Edita um Comercio existente",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Comercio editado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Comercio.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Requisição inválida",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Comercio não encontrado",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno no servidor",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @PutMapping("/edit")
     public ResponseEntity<Comercio> editComercio(@RequestBody Comercio comercio) {
         return ResponseEntity.status(HttpStatus.OK).body(comercioService.updateComercio(comercio));
     }
 
-    @Operation(summary = "Excluir comercio", description = "Exclui um comercio com base no ID fornecido")
+    @Operation(
+            summary = "Excluir comercio",
+            description = "Exclui um comercio com base no ID fornecido",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Comercio excluído com sucesso",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Comercio não encontrado",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Erro interno no servidor",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteComercio(@PathVariable("id") Long id) {
         comercioService.deleteComercio(id);
