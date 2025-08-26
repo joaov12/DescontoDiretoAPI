79d6b1b8858711c17482ed03254afb95f4ee3fd4
Joao Vitor Farias Soares <cpl.jsoares@ifood3rd.com>
2025-08-26 13:25:04 -0300

    fix: Corrige erro ao enviar fotos para o AWS S3

--- src/main/java/com/dd/descontodiretoapi/services/aws/S3Service.java
+++ src/main/java/com/dd/descontodiretoapi/services/aws/S3Service.java
@@ -1,12 +1,8 @@
 package com.dd.descontodiretoapi.services.aws;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
@@ -26,7 +22,6 @@ public class S3Service {
             metadata.setContentLength(fileSize);
             metadata.setContentType(contentType);
 
-            // Fazer upload do arquivo
             s3Client.putObject(bucketName, "comercios/" + fileName, fileStream, metadata);
 
             return s3Client.getUrl(bucketName, "comercios/" + fileName).toString();
@@ -41,7 +36,6 @@ public class S3Service {
             metadata.setContentLength(fileSize);
             metadata.setContentType(contentType);
 
-            
             String objectKey = "produtos/" + fileName; 
 
             s3Client.putObject(bucketName, objectKey, fileStream, metadata);
@@ -57,7 +51,6 @@ public class S3Service {
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentLength(fileSize);
             metadata.setContentType(contentType);
-
 
             String objectKey = "panfletos/" + fileName;
 
