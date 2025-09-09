f0a1634fd5393cc11dedf56fa9d20552206755d4
Yasmine Belmiro <devyasminebelmiro@gmail.com>
2025-09-09 16:59:59 -0300

    refactor: configurando o cors para ser compativel com o vite

--- src/main/java/com/dd/descontodiretoapi/config/CorsConfig.java
+++ src/main/java/com/dd/descontodiretoapi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
