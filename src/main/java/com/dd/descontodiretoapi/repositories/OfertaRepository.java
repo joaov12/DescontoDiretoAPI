f7500c9e838dafbca57cb89ac399a75cf3eb0057
João Vitor <jvfs12th@gmail.com>
2025-09-12 21:36:58 -0300

    data: Adiciona queries para consultar top de ofertas

--- src/main/java/com/dd/descontodiretoapi/repositories/OfertaRepository.java
+++ src/main/java/com/dd/descontodiretoapi/repositories/OfertaRepository.java
@@ -2,8 +2,16 @@ package com.dd.descontodiretoapi.repositories;
 
 import com.dd.descontodiretoapi.models.Oferta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OfertaRepository extends JpaRepository<Oferta, Long> {
+    
+    @Query("SELECT o FROM Oferta o LEFT JOIN o.clientes c GROUP BY o.id ORDER BY COUNT(c) DESC")
+    List<Oferta> findTop10ByOrderByLikesDesc();
+    
+    List<Oferta> findTop10ByOrderByDataPostagemDesc();
 }
